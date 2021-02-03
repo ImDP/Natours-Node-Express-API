4099de9f7682bf4079e84a58502a81d9e214095c
Duncan <duncan@dinegigs.com>
2021-02-03 15:13:39 -0500

    nodemon bug fixed

--- starter/app.js
+++ starter/app.js
@@ -8,8 +8,13 @@ app.get('/', (req, res) => {
 })
 
 
+app.post('/', (req, res) => {
+    res.send('You can post to this endpoint...');
+});
+
+
 const port = 3000;
 app.listen(port, () => {
-    console.log('App running on port ${port}...');
+    console.log('App running on port ${port} ...');
 });
 
