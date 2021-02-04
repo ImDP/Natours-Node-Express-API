4f6ed963d3de778433e9c57742eb7c6e9014b951
Duncan <duncan@dinegigs.com>
2021-02-04 15:12:38 -0500

    Post API

--- starter/app.js
+++ starter/app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
 /*
 app.get('/', (req, res) => {
     res.status(200)
@@ -26,8 +28,34 @@ app.get('/api/v1/tours', (req, res) => {
     });
 });
 
+
+app.post('/api/v1/tours', (req, res) => {
+    //console.log(reg.body);
+
+    const newID = tours[tours.length -1].id + 1;
+    const newTour = Object.assign({ id: newID }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`, 
+        JSON.stringify(tours), 
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                tour: newTour
+                }
+            });    
+        }
+    );
+});
+
+
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
+
 
