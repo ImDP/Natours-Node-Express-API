2d69138eb7db0e06ec8e27ced458d1afb622eef9
Duncan <duncan@dinegigs.com>
2021-02-05 12:51:03 -0500

    Delete (204) API

--- starter/app.js
+++ starter/app.js
@@ -86,7 +86,21 @@ app.patch('/api/v1/tours/:id', (req, res) => {
             tour: '<Updated tour here...>'
         }
     })
-})
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+    if (req.params * 1 > tours.length) { 
+      return res.status(404).json({
+          status: 'fail',
+          message: 'Invalid ID'
+      });
+    }  
+      
+    res.status(204).json({
+        status: 'success',
+        data:  null          
+    });
+  });
 
 const port = 3000;
 app.listen(port, () => {
