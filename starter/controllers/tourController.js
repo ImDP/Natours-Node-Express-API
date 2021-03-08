994c27273ab80196b7c07ec5fb0e8572b967759c
Duncan <duncan@dinegigs.com>
2021-03-08 16:15:59 -0500

    refactored API && created APIFeatures

--- starter/controllers/tourController.js
+++ starter/controllers/tourController.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line no-unused-vars
-//const { query } = require('express');
-//const { findById } = require('../models/tourModel');
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
 
 //created router "top-5-cheap" in tourRoutes, added logic here for query
 exports.aliasTopTours = async (req, res, next) => {
@@ -11,88 +10,15 @@ exports.aliasTopTours = async (req, res, next) => {
   next();
 };
 
-/*
-//This is for testing json data
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-*/
-
-/*
-//Middleware check
-exports.checkID = (req, res, next, val) => {
-  // eslint-disable-next-line no-console
-  console.log(`Tour id is: ${val}`);
-
-  if (req.params * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
-*/
-
-/*
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name and/or price',
-    });
-  }
-  next();
-};
-*/
-
 exports.getAllTours = async (req, res) => {
   try {
-    //BUILD QUERY
-    //BASIC FILTERING
-    const queryObj = { ...req.query };
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    //ADVANCED FILTERING
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    //SORTING
-    if (req.query.sort) {
-      // eslint-disable-next-line no-unused-vars
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(req.query.sort);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    //Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    //Pagination
-    const page = req.query.page * 1 || 1; //defaults to page 1
-    const limit = req.query.limit * 1 || 100; //turns string into number && limits number of results
-    const skip = (page - 1) * limit; //current page minus 1 * limit = number of results to skip
-
-    //page=2&limit=10 1-10 Page 1, 11-20 Page 2, 21-30 Page 3 etc.
-    query = query.skip(skip).limit(limit);
-
-    //Throws error if the number of pages requested is > than number of pages that exist
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
     //EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     //SEND RESPONSE
     res.status(200).json({
@@ -127,16 +53,6 @@ exports.getTour = async (req, res) => {
     });
   }
 };
-
-/*
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  }); 
-};*/
 
 exports.createTour = async (req, res) => {
   try {
